04d14960c44a8fe60a69222c94647ab4eee15d11
Petar Ćulav <petar.culav@outlook.com>
2023-10-18 11:13:29 +0200

    initial commit

---
+++ semester-project/drugi_lab/app/page.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <main>
+      <h1 className="flex justify-center p-14 text-4x1"> Home Page</h1>
+    </main>
+  );
+}
+
+export default Home;
---
+++ semester-project/drugi_lab/app/showcase/page.tsx
@@ -0,0 +1,10 @@
+function Showcase()
+{
+    return (
+    <main>
+      <h1 className="flex justify-center p-14 text-4x1"> Showcase Page</h1>
+    </main>
+    );
+}
+
+export default Showcase;
