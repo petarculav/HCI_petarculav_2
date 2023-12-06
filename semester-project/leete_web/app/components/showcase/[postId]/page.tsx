646a38d557e6c7e4be35057303fe5ca09c3fad4b
petarculav <petar.culav@outlook.com>
2023-12-06 09:47:40 +0100

    pagination

--- semester-project/leete_web/app/components/showcase/[postId]/page.tsx
+++ semester-project/leete_web/app/components/showcase/[postId]/page.tsx
@@ -17,7 +17,7 @@ export default async function BlogPost({ params }: { params: Params }) {
   return (
     <main className="flex flex-col items-center min-h-screen max-w-5xl m-auto p-10">
       <h1 className="text-3xl font-bold p-10 capitalize">
-        <span className="text-neutral-400">Post {post.id}:</span> {post.title}
+        <span className="text-neutral-400">Mix {post.id}:</span> {post.title}
       </h1>
       <p className="text-xl p-10">{post.body}</p>
     </main>
