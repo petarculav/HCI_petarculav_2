66d836f669abc86fd33c1fe4ff23df90548288cf
petarculav <petar.culav@outlook.com>
2023-12-06 09:08:43 +0100

    Vjezba 3: Use API to fetch data

--- semester-project/leete_web/app/components/showcase/[postId]/page.tsx
+++ semester-project/leete_web/app/components/showcase/[postId]/page.tsx
@@ -1,15 +1,25 @@
+import { Post } from "../page";
+
 interface Params {
-    postId: string;
-  }
-  
-  export interface BlogPostParams {
-    params: Params;
-  }
-  
-  export default function BlogPost({ params }: BlogPostParams) {
-    return (
-      <main className="flex min-h-screen flex-col items-center p-10">
-        <h1 className="text-3xl font-bold p-10">Mix #{params.postId}</h1>
-      </main>
-    );
-  }
+  postId: string;
+}
+
+const BASE_API_URL = "https://jsonplaceholder.typicode.com";
+
+const getPost = async (id: string): Promise<Post> => {
+  const data = await fetch(`${BASE_API_URL}/posts/${id}`);
+  return data.json();
+};
+
+export default async function BlogPost({ params }: { params: Params }) {
+  const post = await getPost(params.postId);
+
+  return (
+    <main className="flex flex-col items-center min-h-screen max-w-5xl m-auto p-10">
+      <h1 className="text-3xl font-bold p-10 capitalize">
+        <span className="text-neutral-400">Post {post.id}:</span> {post.title}
+      </h1>
+      <p className="text-xl p-10">{post.body}</p>
+    </main>
+  );
+}
--- semester-project/leete_web/app/components/showcase/page.tsx
+++ semester-project/leete_web/app/components/showcase/page.tsx
@@ -1,19 +1,32 @@
 import Link from "next/link";
 
-export const metadata = {
-  title: "LEETE",
+export interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+const BASE_API_URL = "https://jsonplaceholder.typicode.com";
+
+const getPosts = async (): Promise<Post[]> => {
+  const data = await fetch(`${BASE_API_URL}/posts`);
+  return data.json();
 };
 
-const posts = [12, 3, 56, 7, 89];
-
-export default function Blog() {
+export default async function Blog() {
+  const posts = await getPosts();
   return (
-    <main className="flex min-h-screen flex-col items-center p-10">
-      <h1 className="text-3xl font-bold p-10">Showcase of all LEETE's work</h1>
+    <main className="flex flex-col items-center min-h-screen max-w-5xl m-auto p-10">
+      <h1 className="text-3xl font-bold p-10">Mixes</h1>
       <ul className="flex flex-col gap-8">
-        {posts.map((postId) => (
-          <li key={postId}>
-            <Link href={`blog/${postId}`}>Mix {postId}</Link>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link href={`blog/${post.id}`}>
+              <span className="text-2xl text-purple-500">
+                Mix {post.title}
+              </span>
+            </Link>
           </li>
         ))}
       </ul>
