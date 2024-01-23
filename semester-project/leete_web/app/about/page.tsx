d50f151f408d09e994dc3d9824e80bec4f5067ba
petarculav <petar.culav@outlook.com>
2024-01-23 12:33:56 +0100

    adding calendar and booking form

--- semester-project/leete_web/app/about/page.tsx
+++ semester-project/leete_web/app/about/page.tsx
@@ -11,13 +11,18 @@ export default function About() {
             <img src="/2-1@3x.jpg" alt="LEETE" className="about-image" />
                 <div className="about-description">
                     <p>
-                        LEETE, a renowned DJ based in Split, Croatia, has been captivating audiences with his unique blend of electronic beats and mesmerizing rhythms. With a passion for music that resonates with every performance, LEETE has established himself as a prominent figure in the local and international DJ scene.
+                        LEETE, a renowned DJ based in Split, Croatia, has been captivating audiences with his unique blend of 
+                        electronic beats and mesmerizing rhythms. With a passion for music that resonates with every performance, 
+                        LEETE has established himself as a prominent figure in the local and international DJ scene.
                     </p>
                     <p>
-                        His journey began in the vibrant streets of Split, where he discovered his love for music. Over the years, LEETE has honed his skills, experimenting with various genres and creating a signature sound that is both innovative and timeless.
+                        His journey began in the vibrant streets of Split, where he discovered his love for music. Over the years, 
+                        LEETE has honed his skills, experimenting with various genres and creating a signature sound that is both innovative and timeless.
                     </p>
                     <p>
-                        Whether performing at local clubs or international music festivals, LEETEs sets are an immersive experience, taking listeners on a journey through sound and emotion. His dedication to his craft and his ability to connect with his audience make him a must-see act for any music enthusiast.
+                        Whether performing at local clubs or international music festivals, LEETEs sets are an immersive experience, 
+                        taking listeners on a journey through sound and emotion. His dedication to his craft and his ability to connect 
+                        with his audience make him a must-see act for any music enthusiast.
                     </p>
                 </div>
             </section>
